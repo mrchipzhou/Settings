9910dbba8486acb5d61aedc01e0c70aba4f11432
X.F.Zhou <zhouxinfeng@srlab.com.cn>
2019-09-26 14:26:27 +0800

    Add setting for windows.

--- VSCode/QML.CMake/main.cpp
+++ VSCode/QML.CMake/main.cpp
@@ -1,4 +1,6 @@
+#include <QCoreApplication>
 #include <QGuiApplication>
+#include <QObject>
 #include <QQmlApplicationEngine>
 
 int main(int argc, char *argv[])
@@ -11,9 +13,10 @@ int main(int argc, char *argv[])
     const QUrl url(QStringLiteral("qrc:/main.qml"));
     QObject::connect(&engine, &QQmlApplicationEngine::objectCreated,
                      &app, [url](QObject *obj, const QUrl &objUrl) {
-        if (!obj && url == objUrl)
-            QCoreApplication::exit(-1);
-    }, Qt::QueuedConnection);
+                         if (!obj && url == objUrl)
+                             QCoreApplication::exit(-1);
+                     },
+                     Qt::QueuedConnection);
     engine.load(url);
     return app.exec();
 }
