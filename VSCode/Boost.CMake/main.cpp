4bcd93712ec2134d4d0480ea478e84820e49a670
Mr.Chip <xfzhou233@gmail.com>
2019-09-28 14:53:07 +0800

    Add a boost template

---
+++ VSCode/Boost.CMake/main.cpp
@@ -0,0 +1,10 @@
+#include <iostream>
+#include <boost/version.hpp>
+
+using namespace std;
+
+int main() {
+    cout << "Hello, World!" << endl;
+    cout << "Boost版本：" << BOOST_VERSION << endl;
+    return 0;
+}
